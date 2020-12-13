d20bac342715b2a5e055a52b6750450e781b5eb7
Mihyaeru <mihyaeru21@gmail.com>
2020-12-13 17:53:03 +0900

    :sparkles: run test ci

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+    const token: string = core.getInput('github-token')
+
+    if (!token) {
+      throw new Error("'github-token' input missing");
+    }
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
